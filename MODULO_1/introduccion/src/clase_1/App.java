8d717d4ce368b020fd30aa0f994fa70eb1e6d475
Facundo Villalba <vfacu44@gmail.com>
2025-10-15 20:36:51 -0300

    añadiendo proyecto introductorio a java

---
+++ MODULO_1/introduccion/src/clase_1/App.java
@@ -0,0 +1,36 @@
+package clase_1;
+
+public class App {
+
+	public static void main(String[] args) {
+		/* 
+		 Declaración de variables
+		 	Tipo identificador;
+		  
+		 Asignación (Operador = )
+		 	x = 10; 
+		 	es una asignación de drcha. a izqda.*/
+		 	
+		/*Tipo de dato Enteros:
+		 		 	Rango numerico	|Cant. de memoria
+		 	->byte:	-128 a +127 	|( 1 byte )
+		 	->short:	-32678 a +32767 |( 2 bytes )
+		 	->int: -2147483648 		|
+		 				a			|( 4 bytes )
+		 		 +2147483647  		|
+		 	->long:-9223372036854775808|
+		 	 		a				|( 8 bytes) 
+		 	 	 +9223372036854775808|		
+		 */
+		
+		/*Tipo de datos Reales:
+		 		  |Cant. de memoria
+		  ->float: ( 4 bytes )
+		  ->double:( 8 bytes )		 
+		*/
+		
+		System.out.println("hola");
+		
+	}
+
+}
