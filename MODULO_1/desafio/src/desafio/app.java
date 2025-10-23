1474439fb7c367b90657acde1a20c6f2f49264da
Facundo_Villalba <vfacu44@gmail.com>
2025-10-23 12:51:29 -0300

    ejercicio 1 del desafio

---
+++ MODULO_1/desafio/src/desafio/app.java
@@ -0,0 +1,42 @@
+package desafio;
+
+public class app {
+
+	public static void main(String[] args) {
+		// ejercicio 1
+		System.out.println("A");
+		int x = 10;
+		int y = 20;
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("B");
+		x = x + 5;
+		y = y + 10;
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("C");
+		x = x - 5;
+		y = y - 10;
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("D");
+		x = x * 3;
+		y = y * 5;
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("E");
+		x = x / 2;
+		y = y / 4;
+		System.out.println(x);
+		System.out.println(y);
+		//ejercicio 2
+		
+		
+
+	}
+
+}
