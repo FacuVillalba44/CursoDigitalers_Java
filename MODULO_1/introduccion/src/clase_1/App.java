12eca2a9e9a39ab9de1b476a72c7954ef966d600
Facundo_Villalba <vfacu44@gmail.com>
2025-10-23 09:42:04 -0300

    notas sobre variables

--- MODULO_1/introduccion/src/clase_1/App.java
+++ MODULO_1/introduccion/src/clase_1/App.java
@@ -25,9 +25,35 @@ public class App {
 		
 		/*Tipo de datos Reales:
 		 		  |Cant. de memoria
-		  ->float: ( 4 bytes )
-		  ->double:( 8 bytes )		 
+		  ->float: ( 4 bytes )/presición de 8 dígitos
+		  ->double:( 8 bytes )/presición de 16 digitos
+		  ej: float numReal;
+		  	  numReal=12.34567890f;<- para que lo reconozca como float agregar "f" si tiene punto
+		  	 double numReal2;
+		  	 numReal2=12.3456789;
 		*/
+		/*Tipos de dato para caracteres
+		  	char (cada caracter tiene un número asociado)
+		  	el contenido asignado a entre comillas simples
+		  	admite de a UN caracter
+		  	ej: char letra = 'h';
+		  	se pueden asignar numeros y en pantalla se vera el caracter asociado		 
+		 * */
+		/*Clase de dato String: cadena de caracteres
+	  		String (admite grupos de caracteres)
+	  		el contenido asignado a entre comillas dobles
+	  		ej: String txt = "hola mundo"		 
+		 */
+		/*Tipo de datos Logicos:
+		 	boolean: solo almacena "true" o "false"		  
+		  */
+		
+		/*Declaración de constantes:
+		 * final tipo identificador = valor;
+		 ej: final float PI = 3.14f;
+		 * lleva la palabra "final" al inicio
+		 * Se debe asignar en la misma linea 
+		 */
 		
 		System.out.println("hola");
 		
